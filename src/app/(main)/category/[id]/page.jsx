05abe7c370c393af3958aa4ab0f92ace54bafc85
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-05-14 20:51:03 +0600

    news data loaded again

--- src/app/(main)/category/[id]/page.jsx
+++ src/app/(main)/category/[id]/page.jsx
@@ -1,17 +1,8 @@
 import LeftSidebar from '@/components/homepage/news/LeftSidebar';
 import RightSidebar from '@/components/homepage/news/RightSidebar';
+import { getCategories, getNewsByCategoriesId } from '@/lib/data';
 import React from 'react';
 
-const getCategories = async () => {
-    const res = await fetch("https://openapi.programming-hero.com/api/news/categories")
-    const data = await res.json()
-    return data.data;
-}
-const getNewsByCategoriesId = async (category_id) => {
-    const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`)
-    const data = await res.json()
-    return data.data;
-}
 
 const NewsCategoryPage = async ({ params }) => {
     const { id } = await params;
@@ -27,9 +18,9 @@ const NewsCategoryPage = async ({ params }) => {
             </div>
 
 
-            <div className="col-span-6   bg-purple-300">
-                All news
-                <div className="space-y-2">
+            <div className="col-span-6 ">
+                <h2>New by category</h2>
+                <div className="space-y-2 mt-6">
                     {
                         news.length > 0 ? news.map(n => {
                             return <div className="border p-4 m-2 rounded-xl" key={n._id}>
--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,50 +1,8 @@
-import LeftSidebar from "@/components/homepage/news/LeftSidebar";
-import RightSidebar from "@/components/homepage/news/RightSidebar";
+import { redirect } from "next/navigation"
 
 
-const getCategories = async () => {
-  const res = await fetch("https://openapi.programming-hero.com/api/news/categories")
-  const data = await res.json()
-  return data.data;
-}
-const getNewsByCategoriesId = async (category_id) => {
-  const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`)
-  const data = await res.json()
-  return data.data;
-}
-
+const default_category_id = "01"
 
 export default async function Home() {
-  const categories = await getCategories()
-  const news = await getNewsByCategoriesId("01")
-
-
-  return (
-    <div className="grid grid-cols-12 gap-2 my-[60px]">
-
-      {/* leftsidebar  */}
-      <div className="col-span-3  ">
-        <LeftSidebar categories={categories} activeId={"01"}></LeftSidebar>
-      </div>
-
-
-      <div className="col-span-6   bg-purple-300">
-        All news
-        <div className="space-y-2">
-          {
-            news.map(n => {
-              return ( 
-              <div className="border p-4 m-2 rounded-xl" key={n._id}>
-                {n.title}
-              </div>
-              )
-            }) 
-          }
-        </div>
-      </div>
-      <div className="col-span-3  ">
-        <RightSidebar></RightSidebar>
-      </div>
-    </div>
-  );
+   redirect(`/category/${default_category_id}`)
 }
